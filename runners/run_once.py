9b2c05aca57fc66ef4a96069cb7347dc4aca96b2
Architect89 <Architect89@proton.me>
2025-12-25 15:46:48 +0530

    Update run_once.py

--- runners/run_once.py
+++ runners/run_once.py
@@ -5,3 +5,8 @@ from actions.probe_action import ProbeAction
 
 loop = LifeLoop(ActionExecutor(), Logger())
 loop.run_experiment(ProbeAction())
+def run():
+    # whatever the script does in __main__
+    # move that logic here
+    main_record = run_probe()   # or whatever your one-shot call is
+    return main_record
