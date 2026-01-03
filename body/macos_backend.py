4ef5619494ea081be9880ff11f4d308ddc9dd95c
Architect89 <Architect89@proton.me>
2026-01-03 14:58:57 +0530

    Delete body/macos_backend.py

--- body/macos_backend.py
+++
@@ -1,7 +0,0 @@
-from execution.backend_contract import BackendBase
-from core.poison import Poison
-
-
-class MacOSBackend(BackendBase):
-    def __init__(self, *args, **kwargs):
-        Poison.trigger("macOS backend is not supported in this build")
